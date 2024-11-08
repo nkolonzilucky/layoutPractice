dc5f17bbb7a642cac12cbc6bb5958ce125308a37
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-08 05:07:06 +0000

    init commit

---
+++ layou-tapp/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const tailwindcssanimate = require('tailwindcss-animate')
+
+const config: Config = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [
+    daisyui,
+    tailwindcssanimate
+],
+};
+export default config;
