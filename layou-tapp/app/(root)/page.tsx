a09e48122a6524f94968950dd6a932958db36284
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-08 07:02:37 +0000

    created diffrence layout for different routes

--- layou-tapp/app/(root)/page.tsx
+++ layou-tapp/app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Hello from "./component/Hello";
 import Link from "next/link";
+import Hello from "../component/Hello";
 
 export default function Home() {
   console.log("Server or client")
